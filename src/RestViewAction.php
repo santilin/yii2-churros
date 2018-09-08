97f2b14f0369713b6396bbbbca5e329ba7963956
Santilin <software@noviolento.es>
2018-09-08 17:05:55 +0100

    Prepared for use with Vue (requests and response headers)

---
+++ src/RestViewAction.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace santilin\Churros;
+
+use Yii;
+use yii\rest\ViewAction;
+
+/**
+ * ViewAction implements the API endpoint for returning the detailed information about a model.
+ *
+ * For more details and usage information on ViewAction, see the [guide article on rest controllers](guide:rest-controllers).
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class RestViewAction extends ViewAction
+{
+    /**
+     * Displays a model.
+     * @param string $id the primary key of the model.
+     * @return \yii\db\ActiveRecordInterface the model being displayed
+     */
+    public function run($id)
+    {
+		if ($id == 0 ) {
+			/* @var $model \yii\db\ActiveRecord */
+			$model = new $this->modelClass();
+			$model->setDefaultValues();
+			return $model;
+		} else {
+			return parent::run($id);
+		}
+    }
+}
