6ceca4fa582b71703b9429bf4bd0430729ee5693
Santilín <software@noviolento.es>
2022-09-07 18:37:25 +0200

    ++ reportview: pdf

--- src/ReportsControllerTrait.php
+++ src/ReportsControllerTrait.php
@@ -6,6 +6,9 @@ use Yii;
 use yii\base\ViewNotFoundException;
 use yii\web\Response;
 
+use santilin\churros\helpers\AppHelper;
+use santilin\churros\grid\ReportView;
+
 trait ReportsControllerTrait
 {
 
@@ -110,12 +113,23 @@ trait ReportsControllerTrait
 		// Merge the post params and replace the saved ones
 		$report->load($params);
 		try {
-			return $this->render('report', [
-				'report' => $report,
-				'reportModel' => $search_model,
-				'params' => $params,
-				'extraParams' => $this->changeActionParams($params, 'report', $report)
-			]);
+			if( isset($params['pdf']) ) {
+				$content = $this->renderPartial('report', [
+					'report' => $report,
+					'reportModel' => $search_model,
+					'params' => $params,
+					'extraParams' => $this->changeActionParams($params, 'report', $report)
+				]);
+				$this->sendPdf($report, $content);
+				die;
+			} else {
+				return $this->render('report', [
+					'report' => $report,
+					'reportModel' => $search_model,
+					'params' => $params,
+					'extraParams' => $this->changeActionParams($params, 'report', $report)
+				]);
+			}
 		} catch( \yii\base\InvalidArgumentException $e ) {
 			Yii::$app->session->setFlash('error',
 				$report->t('churros', 'The report "{record}" has definition errors'));
@@ -124,6 +138,65 @@ trait ReportsControllerTrait
 	}
 
 
+	protected function sendPdf($report, $content)
+	{
+		$pdfHeader=<<<EOF
+<table width="100%" height="48px" style="vertical-align: bottom; font-family: serif;
+	font-size: 8pt; color: #000000; font-weight: bold; font-style: italic;">
+	<tr>
+		<td width="20%"><img height='48px' src='/img/logo_icono.jpg'/></td>
+		<td width="" style="text-align: center;">{$report->title}</td>
+		<td width="20%" align="right">{DATE j-m-Y} - {PAGENO}/{nbpg}</td>
+	</tr>
+</table>
+EOF;
+		$pdfFooter = null;
+		if( YII_DEBUG ) {
+            Yii::$app->getModule('debug')->instance->allowedIPs = [];
+        }
+		$methods = [];
+		$margin_top = AppHelper::yiiparam('pdfMarginTop', 30); // Inicio del contenido de la página sin header desde el borde superior de la página
+		$margin_bottom = AppHelper::yiiparam('pdfMarginBottom', 20);
+		$margin_header = AppHelper::yiiparam('pdfMarginHeader', 10); // Inicio del header desde el borde superior de la página
+		$margin_footer = AppHelper::yiiparam('pdfMarginFooter', 15);
+		if( $this->findViewFile('_report_header') ) {
+			$header_content = $this->renderPartial('_report_header', ['model'=>$report]);
+			// h:{00232}
+			if( strncmp($header_content,'h:{',3) === 0 ) {
+				$margin_top = intval(substr($header_content,3,5));
+				$header_content = substr($header_content,9);
+			}
+			$methods['setHeader'] = $header_content;
+		} else {
+// 			$methods['setHeader'] = date('Y-m-d H:i') . '|'
+// 				. $model->getModelInfo('title') . '|' . Yii::$app->name . ' - {PAGENO}';
+			$methods['setHeader'] = $pdfHeader;
+ 			$margin_top = 30;
+ 			$margin_header = 10;
+		}
+		if( $this->findViewFile('_pdf_footer') ) {
+			$methods['setFooter'] = $this->renderPartial('_pdf_footer', ['model'=>$model]);
+		} else {
+			$methods['setFooter'] = $pdfFooter;
+		}
+		$pdf = new \kartik\mpdf\Pdf([
+			'mode' => \kartik\mpdf\Pdf::MODE_CORE,
+			'format' => \kartik\mpdf\Pdf::FORMAT_A4,
+			'orientation' => \kartik\mpdf\Pdf::ORIENT_PORTRAIT,
+			'destination' => \kartik\mpdf\Pdf::DEST_DOWNLOAD,
+			'marginHeader' => $margin_header, // Margin from top of page
+			'marginFooter' => $margin_footer, // Margin from bottom of page
+			'marginTop' => $margin_top, // Margin from top of page to content
+			'marginBottom' => $margin_bottom, // $margin_footer,
+			'content' => $content,
+// 			'cssFile' => '@vendor/kartik-v/yii2-mpdf/src/assets/kv-mpdf-bootstrap.min.css',
+			'cssInline' => file_get_contents(Yii::getAlias('@app') . '/web/css/print.css'),
+			'options' => ['title' => $report->recordDesc()],
+			'methods' => $methods,
+		]);
+ 		$pdf->filename = "/home/santilin/devel/tmp/pdf.pdf";
+		return $pdf->render();
+	}
 
 } // class SiteController
 
