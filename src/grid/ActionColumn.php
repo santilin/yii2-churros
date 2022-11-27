1cfef479f97c9b18fc547d1d22a8eab8c2664a2c
Santilín <software@noviolento.es>
2022-11-27 10:48:42 +0100

    Fix: ActionColumn

--- src/grid/ActionColumn.php
+++ src/grid/ActionColumn.php
@@ -17,53 +17,53 @@ class ActionColumn extends \yii\grid\ActionColumn
     public $crudPerms = null;
     public $deleteOptions = [];
     public $hAlign = 'none';
+
     /**
      * Initializes the default button rendering callbacks.
      */
-//     protected function initDefaultButtons()
-//     {
-// 		$this->hAlign = 'left';
-//         $notBs3 = true; // !$this->grid->isBs(3);
-//         if( $this->crudPerms === null ) {
-// 			$this->crudPerms = 'CRUD2';
-// 		}
-//         if( strpos($this->crudPerms,'R') !== false ) {
-// 			if( isset($this->customButtons['view']) ) {
-// 				$this->buttons['view'] = $this->customButtons['view'];
-// 				unset( $this->customButtons['view'] );
-// 			} else {
-// 				$this->setDefaultButton('view', Yii::t('churros', 'View'), $notBs3 ? 'eye' : 'eye-open');
-// 			}
-// 		}
-//         if( strpos($this->crudPerms,'U') !== false ) {
-// 			if( isset($this->customButtons['update']) ) {
-// 				$this->buttons['update'] = $this->customButtons['update'];
-// 				unset( $this->customButtons['update'] );
-// 			} else {
-// 				$this->setDefaultButton('update', Yii::t('churros', 'Update'), $notBs3 ? 'pencil-alt' : 'pencil');
-// 			}
-// 		}
-//         if( strpos($this->crudPerms,'D') !== false ) {
-// 			if( isset($this->customButtons['delete']) ) {
-// 				$this->buttons['delete'] = $this->customButtons['delete'];
-// 				unset( $this->customButtons['delete'] );
-// 			} else {
-// 				$this->setDefaultButton('delete', Yii::t('churros', 'Delete'), $notBs3 ? 'trash-alt' : 'trash');
-// 			}
-// 		}
-//         if( strpos($this->crudPerms,'2') !== false ) {
-// 			if( isset($this->customButtons['duplicate']) ) {
-// 				$this->buttons['duplicate'] = $this->customButtons['duplicate'];
-// 				unset( $this->customButtons['duplicate'] );
-// 			} else {
-// 				$this->setDefaultButton('duplicate', Yii::t('churros', 'Duplicate'), $notBs3 ? 'copy' : 'duplicate');
-// 			}
-// 		}
-// 		foreach( $this->customButtons as $index => $button ) {
-// 			$this->template .= '&nbsp;{' . $index. '}';
-// 			$this->buttons[$index] = $button;
-// 		}
-// 		$this->customButtons = []; // https://github.com/kartik-v/yii2-grid/issues/1047
-//     }
+    protected function initDefaultButtons()
+    {
+		$this->hAlign = 'left';
+        if( $this->crudPerms === null ) {
+			$this->crudPerms = 'CRUD2';
+		}
+        if( strpos($this->crudPerms,'R') !== false ) {
+			if( isset($this->customButtons['view']) ) {
+				$this->buttons['view'] = $this->customButtons['view'];
+				unset( $this->customButtons['view'] );
+			} else {
+				$this->initDefaultButton('view', 'eye-open', [ 'title' => Yii::t('churros', 'View')]);
+			}
+		}
+        if( strpos($this->crudPerms,'U') !== false ) {
+			if( isset($this->customButtons['update']) ) {
+				$this->buttons['update'] = $this->customButtons['update'];
+				unset( $this->customButtons['update'] );
+			} else {
+				$this->initDefaultButton('update', 'pencil-alt', [ 'title' => Yii::t('churros', 'Update')]);
+			}
+		}
+        if( strpos($this->crudPerms,'D') !== false ) {
+			if( isset($this->customButtons['delete']) ) {
+				$this->buttons['delete'] = $this->customButtons['delete'];
+				unset( $this->customButtons['delete'] );
+			} else {
+				$this->initDefaultButton('delete', 'trash-alt', [ 'title' => Yii::t('churros', 'Delete')]);
+			}
+		}
+        if( strpos($this->crudPerms,'2') !== false ) {
+			if( isset($this->customButtons['duplicate']) ) {
+				$this->buttons['duplicate'] = $this->customButtons['duplicate'];
+				unset( $this->customButtons['duplicate'] );
+			} else {
+				$this->initDefaultButton('duplicate', 'copy', [ 'title' => Yii::t('churros', 'Duplicate')]);
+			}
+		}
+		foreach( $this->customButtons as $index => $button ) {
+			$this->template .= '&nbsp;{' . $index. '}';
+			$this->buttons[$index] = $button;
+		}
+		$this->customButtons = []; // https://github.com/kartik-v/yii2-grid/issues/1047
+    }
 
 }
