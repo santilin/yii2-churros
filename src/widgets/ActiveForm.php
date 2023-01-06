7aa99db68fbe4d9c24cd466a4df3a81058f28d54
Santilín <software@noviolento.es>
2023-01-06 09:34:36 +0100

    ActiveForm: - layoutForm: lays out fields and buttons - layoutContent: layus out free content

--- src/widgets/ActiveForm.php
+++ src/widgets/ActiveForm.php
@@ -1,13 +1,13 @@
 <?php
 namespace santilin\churros\widgets;
 
-use yii\helpers\Html;
+use yii\helpers\{ArrayHelper,Html};
 use yii\bootstrap4\ActiveForm as Bs4ActiveForm;
 use santilin\churros\helpers\FormHelper;
 
 class ActiveForm extends Bs4ActiveForm
 {
-
+	public $fieldsLayout;
     public $fieldConfig = [
 		'horizontalCssClasses' => [
 			'offset' => ['col-lg-10 col-md-10 col-sm-9 col-xs-12', 'offset-lg-2 offset-md-2 offset-sm-3 offset-xs-0'],
@@ -19,65 +19,125 @@ class ActiveForm extends Bs4ActiveForm
 		]
     ];
 
-	public function layoutFields($form_fields)
+	public function layoutForm($form_fields, $buttons)
 	{
 		$ret = '';
-		if ($this->layout == "horizontal" || $this->layout == '1col' || $this->layout == "inline" ) {
+		if( empty($this->fieldsLayout) ) {
 			foreach( $form_fields as $name => $code ) {
 				$ret .= $form_fields[$name]. "\n";
 			}
-		} else if( count($form_layout_rows) ) {
-			// Check if some fields have been removed after setting the layout
-			foreach($form_layout_rows as $lrowkey => $lrow ) {
-				foreach( $lrow as $ffkey => $ff ) {
+			return $ret;
+		}
+		// Check if some fields or buttons have been removed after setting the layout
+		foreach($this->fieldsLayout as $layout) {
+			switch( $layout['type'] ) {
+			case 'subtitle':
+			case 'tabs':
+				break;
+			case 'buttons':
+				foreach( $buttons as $bkey => $b ) {
+					if( $buttons[$bkey] === false ) {
+						unset( $buttons[$bkey] );
+					}
+				}
+				break;
+			default:
+				foreach( $layout['fields']??[] as $ffkey => $ff ) {
 					if( $form_fields[$ff] === false ) {
-						unset( $form_layout_rows[$lrowkey][$ffkey] );
+						unset( $this->fieldsLayout[$lrowkey][$ffkey] );
 					}
 				}
 			}
-			foreach($form_layout_rows as $lrow ) {
-				switch(count($lrow)) {
-				case 1:
+		}
+		foreach($this->fieldsLayout as $layout ) {
+			switch( $layout['type'] ) {
+			case 'buttons':
+				if( empty($buttons['buttons']) ) {
+					$ret .= $this->layoutButtons($buttons);
+				} else {
+					$layout_buttons = [];
+					foreach( $buttons['buttons'] as $bkey => $b ) {
+						$layout_buttons[$bkey] = $b;
+					}
+					$ret .= $this->layoutButtons($layout_buttons);
+				}
+				break;
+			case '1col_rows':
+				foreach( $layout['fields'] as $form_field ) {
 					$ret .= '<div class="row">';
 					$ret .= '<div class="col-sm-12">';
-					$ret .= $form_fields[$lrow[0]];
+					$ret .= $form_fields[$form_field];
 					$ret .= '</div>';
 					$ret .= '</div>';
-					break;
-				case 2:
-					$ret .= '<div class="row">';
-					$ret .= '<div class="col-sm-6">';
-					$ret .= $form_fields[$lrow[0]];
-					$ret .= '</div>';
-					$ret .= '<div class="col-sm-6">';
-					$ret .= $form_fields[$lrow[1]];
-					$ret .= '</div>';
-					$ret .= '</div>';
-					break;
-				case 3:
-					$ret .= '<div class="row">';
-					$ret .= '<div class="col-sm-4">';
-					$ret .= $form_fields[$lrow[0]];
-					$ret .= '</div>';
-					$ret .= '<div class="col-sm-4">';
-					$ret .= $form_fields[$lrow[1]];
-					$ret .= '</div>';
-					$ret .= '<div class="col-sm-4">';
-					$ret .= $form_fields[$lrow[2]];
-					$ret .= '</div>';
-					$ret .= '</div>';
-					break;
-				case 4:
-					$ret .= '<div class="row">';
-					$ret .= $form_fields[$lrow[0]];
-					$ret .= $form_fields[$lrow[1]];
-					$ret .= $form_fields[$lrow[2]];
-					$ret .= $form_fields[$lrow[3]];
-					$ret .= '</div>';
-					break;
 				}
+				break;
+			case '2col_rows':
+				$nf = 0;
+				foreach( $layout['fields'] as $form_field ) {
+					if( $nf == 0 ) {
+						$nf = 1;
+						$ret .= '<div class="row">';
+						$ret .= '<div class="col-sm-6">';
+						$ret .= $form_fields[$form_field];
+						$ret .= '</div>';
+					} else {
+						$nf = 0;
+						$ret .= '<div class="col-sm-6">';
+						$ret .= $form_fields[$form_field];
+						$ret .= '</div>';
+						$ret .= '</div>';
+					}
+				}
+				if( $nf == 1 ) {
+					$ret .= '</div>';
+				}
+				break;
+// 			case 3:
+// 				$ret .= '<div class="row">';
+// 				$ret .= '<div class="col-sm-4">';
+// 				$ret .= $form_fields[$lrow[0]];
+// 				$ret .= '</div>';
+// 				$ret .= '<div class="col-sm-4">';
+// 				$ret .= $form_fields[$lrow[1]];
+// 				$ret .= '</div>';
+// 				$ret .= '<div class="col-sm-4">';
+// 				$ret .= $form_fields[$lrow[2]];
+// 				$ret .= '</div>';
+// 				$ret .= '</div>';
+// 				break;
+// 			case 4:
+// 				$ret .= '<div class="row">';
+// 				$ret .= $form_fields[$lrow[0]];
+// 				$ret .= $form_fields[$lrow[1]];
+// 				$ret .= $form_fields[$lrow[2]];
+// 				$ret .= $form_fields[$lrow[3]];
+// 				$ret .= '</div>';
+// 				break;
+			case 'tabs':
+				break;
+			case 'subtitle':
+				$ret .= $this->layoutContent(null, $layout['title'], $layout['options']??[]);
+				break;
 			}
 		}
+		return $ret;
+	}
+
+	public function layoutContent(?string $label, string $content, array $options = []):string
+	{
+		$ret = '';
+		$wrapper_options = [ 'class' => $this->fieldConfig['horizontalCssClasses']['wrapper'] ];
+// 		Html::addCssClass($config['labelOptions'], $cssClasses['label']);
+// 		Html::addCssClass($config['errorOptions'], $cssClasses['error']);
+// 		Html::addCssClass($config['hintOptions'], $cssClasses['hint']);
+// 		Html::addCssClass($config['options'], $cssClasses['field']);
+		if( empty($label) ) {
+			Html::addCssClass($wrapper_options, $this->fieldConfig['horizontalCssClasses']['offset']);
+		}
+		$wrapper_tag = ArrayHelper::remove($wrapper_options, 'tag', 'div');
+		$ret .= Html::beginTag($wrapper_tag, $wrapper_options);
+		$ret .= $content;
+		$ret .= Html::endTag($wrapper_tag);
 		return $ret;
 	}
 
