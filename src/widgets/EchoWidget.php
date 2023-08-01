b91bb253960a2e4807c52eb2c31992359c4f12db
Santilín <software@noviolento.es>
2023-08-01 19:53:31 +0200

    EchoWidget: bs4 classes

--- src/widgets/EchoWidget.php
+++ src/widgets/EchoWidget.php
@@ -12,13 +12,13 @@ class EchoWidget extends \yii\widgets\InputWidget
 		$classes = explode(' ', $this->options['class']);
 		foreach ($classes as $key => $class) {
 			if ($class=='form-control') {
-				$classes[$key] = 'form-control-static';
+				$classes[$key] = 'form-control-plaintext';
 			}
 		}
 		if (empty($classes)) {
-			$classes[] = 'form-control-static';
+			$classes[] = 'form-control-plaintext';
 		}
-		$s_classes = implode(',', $classes);
+		$s_classes = implode(' ', $classes);
 		return "<div class=\"$s_classes\">{$this->value}</div>";
 	}
 }
