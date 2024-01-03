a4e648994bf44c45fa80c28209ad17b759ea8015
santilin <franciscocapel@cepaim.org>
2024-01-03 15:02:53 +0100

    + Widgets/StaticControl

---
+++ src/widgets/StaticControl.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace santilin\churros\widgets;
+
+use Yii;
+use yii\helpers\Html;
+
+class StaticControl extends \yii\widgets\InputWidget
+{
+	public $value = null;
+
+	public function run()
+	{
+		if ($this->value !== null) {
+			$value = $this->value;
+		} else {
+			if (isset($this->options['value'])) {
+				$value = $this->options['value'];
+				if (is_callable($value)) {
+					$value = call_user_func($value);
+				}
+				unset($this->options['value']);
+			} else {
+				$value = Html::getAttributeValue($this->model, $this->field->attribute);
+			}
+		}
+        Html::addCssClass($this->options, 'form-control-plaintext');
+        $value = (string)$value;
+		return Html::tag('div', $value, $this->options);
+	}
+}
