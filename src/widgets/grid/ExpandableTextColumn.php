8abfd2f8c76c3c0c8d18c089227f473c7002ac67
santilin <franciscocapel@cepaim.org>
2024-01-17 08:17:10 +0100

    ExpandableTextColumn: css and code por bs5

--- src/widgets/grid/ExpandableTextColumn.php
+++ src/widgets/grid/ExpandableTextColumn.php
@@ -14,11 +14,12 @@ class ExpandableTextColumn extends DataColumn
      * Maximun text length
      * @var
      */
-    public $length = 30;
+    public $length = 40;
     public $format = 'text';
-	public $captionOptions = []; // @todo
+	public $captionOptions = ['class' => 'see-more-content'];
+	public $contentOptions = ['class' => 'see-more-container'];
 	public $modalBodyOptions = [];
-        public $titulo = "Título";
+	public $title = "Title";
 
     /**
      * {@inheritdoc}
@@ -34,41 +35,54 @@ class ExpandableTextColumn extends DataColumn
 		if( $this->format == 'html' ) {
 			$text = html_entity_decode(strip_tags($text));
 		}
-		if( $this->length == 0 ) {// || strlen($text)<=$this->length) {
+                if( $this->length == 0 ) {// || strlen($text)<=$this->length) {
 			return $text;
 		} else {
+			/// @todo partir por el espacio más próximo
 			$truncated_text = trim(mb_substr(trim($text), 0, $this->length));
-			$encoded_text = Html::encode($text); // Html::tag('p', Html::encode($text), $this->modalBodyOptions);
+			$encoded_text = Html::tag('p', Html::encode($text), $this->modalBodyOptions);
 			$modal = <<<modal
-<div class="modal fade" id="modalSeeMore" tabindex="-1" aria-labelledby="modalSeeMore" style="display: none;" aria-hidden="true">
+<div class="modal fade" id="modalSeeMore_$key" tabindex="-1" aria-labelledby="modalSeeMore_$key" style="display: none;" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-md">
 		<div class="modal-content">
 			<div class="modal-header bg-primary text-white">
-				<h1 class="modal-title fs-5" id="modalLeerMasTitle">$this->titulo</h1>
+				<h1 class="modal-title fs-5" id="modalLeerMasTitle_$key">$this->title</h1>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
-				<div id="modalLeerMasContenido"></div>
-				$encoded_text
+				<div id="modalLeerMasContenido_$key">
+                                    $encoded_text
+                                </div>
 
 			</div>
 			<div class="modal-footer">
-				<button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="modalLeerMasBtn">Copiar</button>
+				<button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="modalLeerMasBtn_$key"><i class="bi bi-clipboard-plus"></i></button>
 			</div>
 		</div>
 	</div>
 </div>
+<script>
+let generateQuoteBtn_$key = document.querySelector('#modalLeerMasBtn_$key');
+//function copyModalTextToClipboard() {
+generateQuoteBtn_$key.addEventListener('click', () => {
+        let contenedor = document.querySelector('#modalLeerMasContenido_$key');
+	let text = contenedor.textContent;
+        text = text.trim();
+        copyToClipboard(text);
 
+	alert("Texto copiado al portapapeles: \\n" + text);
+});
+</script>
 modal;
 
-			$text = Html::tag('span', $truncated_text, $this->captionOptions) . Html::a('<i class="bi bi-arrow-right-circle"></i>', '#', [
+			$text =  Html::tag('span', $truncated_text, $this->captionOptions) . Html::a('<i class="bi bi-arrow-right-circle"></i>', '#', [
 				'title' => 'Pincha para leer más',
 				'class' => "btn btn-outline-primary btn-sm",
 				'data' => [
 					'bs-toggle' => 'modal',
-					'bs-target' => '#modalSeeMore'
+					'bs-target' => '#modalSeeMore_' . $key,
 				],
-				'style' => 'position: absolute; right: 0;',
+                                'style' => 'position: absolute; right: 0;',
 			]);
 			return $modal . $text;
 		}
