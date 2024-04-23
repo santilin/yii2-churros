e6e37b13c750471eb9c46ed7fa727941755f8a82
santilin <franciscocapel@cepaim.org>
2024-04-23 19:56:03 +0200

    Added success messages to ModelInfoTrait

--- src/ModelInfoTrait.php
+++ src/ModelInfoTrait.php
@@ -12,7 +12,7 @@ trait ModelInfoTrait
     /**
      * @var array validation warnings (attribute name => array of warnings)
      */
-    private $_warnings;
+    private $_warnings = null, $_successes = null;
     protected $crudScenarios = [ 'default', 'create', 'duplicate', 'update' ];
 
 
@@ -568,6 +568,71 @@ trait ModelInfoTrait
 
         return $query->andWhere($condition);
 	}
+
+    /**
+     * Returns a value indicating whether there is any success after saving the model
+     * @return bool whether there is any success.
+     */
+    public function hasSuccesses()
+    {
+        return !empty($this->_successes);
+    }
+
+    /**
+     * Returns the successs
+     * @return array successes for the model. Empty array is returned if no success.
+     * See [[getSuccesses()]] for detailed description.
+     *
+     * @see getFirstSuccess()
+     */
+    public function getSuccesses()
+    {
+		return $this->_successes === null ? [] : $this->_successes;
+	}
+
+    /**
+     * Returns the first success
+     * @return string|null the success message. Null is returned if no success.
+     * @see getSuccesses()
+     * @see getFirstSuccesses()
+     */
+    public function getFirstSuccess()
+    {
+        return count($this->_successes) ? reset($this->_successes) : null;
+    }
+
+    /**
+     * Adds a new success to the specified attribute.
+     * @param string $success new success message
+     */
+    public function addSuccess(string $success = '')
+    {
+        $this->_successes[] = $success;
+    }
+
+    /**
+     * Adds a list of successs.
+     * @param array $items a list of successs. * The array values should be success messages.
+     * You may use the result of [[getSuccesses()]] as the value for this parameter.
+     */
+    public function addSuccesses(array $items)
+    {
+		$this->_successes += $items;
+    }
+
+	public function addSuccessesFrom(ModelInfoTrait $model)
+	{
+		$this->addSuccesses($model->getSuccesses);
+	}
+
+
+    /**
+     * Removes all successes
+     */
+    public function clearSuccesses($attribute = null)
+    {
+		$this->_successes = null;
+    }
 
     /**
      * Returns a value indicating whether there is any validation warning.
