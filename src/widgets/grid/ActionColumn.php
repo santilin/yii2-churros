79922554ad82f2402bc7a13006fcd651e007eb42
Santilín <software@noviolento.es>
2024-07-28 23:40:29 +0200

    removed customButtons property

--- src/widgets/grid/ActionColumn.php
+++ src/widgets/grid/ActionColumn.php
@@ -15,7 +15,6 @@ use santilin\churros\helpers\FormHelper;
 class ActionColumn extends \yii\grid\ActionColumn
 {
     public $template = '{view}&nbsp;{update}&nbsp;{delete}&nbsp;{duplicate}';
-    public $customButtons = [];
     public $crudPerms = [];
     public $iconClassPrefix = 'bi bi';
 	public $icons = [
