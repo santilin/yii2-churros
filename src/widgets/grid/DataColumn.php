0271fd0462f20e9ca88c167fe0b10c3148fc2e69
Santilín <software@noviolento.es>
2024-10-05 23:08:30 +0200

    try/catch on renderDataCell to show array to string conversions info

--- src/widgets/grid/DataColumn.php
+++ src/widgets/grid/DataColumn.php
@@ -6,7 +6,7 @@ use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use yii\data\ArrayDataProvider;
 use yii\db\ActiveQueryInterface;
-use yii\helpers\{Html,Inflector};
+use yii\helpers\{ArrayHelper,Html,Inflector};
 
 
 class DataColumn extends \yii\grid\DataColumn
@@ -64,5 +64,24 @@ class DataColumn extends \yii\grid\DataColumn
 
         return $label;
     }
+
+    /**
+     * {@inheritdoc}
+     * Inherited to show the column thas has an error
+     */
+    protected function renderDataCellContent($model, $key, $index)
+    {
+try {
+        if ($this->content === null) {
+            return $this->grid->formatter->format($this->getDataCellValue($model, $key, $index), $this->format);
+        }
+
+        return parent::renderDataCellContent($model, $key, $index);
+} catch (\yii\base\ErrorException $e) {
+    throw new \yii\base\ErrorException($e->getMessage() . ":{$this->attribute}=" . json_encode($model->{$this->attribute}),
+        $e->getCode(), $e->getSeverity(), $e->getFile(), $e->getLine(), $e->getPrevious());
+}
+    }
+
 
 }
