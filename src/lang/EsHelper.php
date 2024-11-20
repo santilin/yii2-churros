87870a83735a379d8c76d0b6fc8f5e9b20bd39e5
Santilín <software@noviolento.es>
2024-11-20 18:59:09 +0100

    added src/lang/EsHelper.php

---
+++ src/lang/EsHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace santilin\churros\lang;
+
+class EsHelper
+{
+	const SPANISH_MALE_WORDS = [
+		"a" => "o",
+		"as" => "os",
+		"as" => "os",
+		"la" => "el",
+		"La" => "El",
+		"las" => "los",
+		"Las" => "Los",
+		"una" => "un",
+		"un_a" => "uno",
+		"Una" => "Un",
+		"Un_a" => "Uno",
+		"esta" => "este",
+		"Esta" => "Este",
+		"estas" => "estos",
+		"Estas" => "Estos",
+		"otra" => "otro",
+		"Otra" => "Otra",
+		"otras" => "otras",
+		"Otras" => "Otras",
+	];
+
+}
