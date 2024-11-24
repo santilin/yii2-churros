ae33c50e032e532260d9750eed0f6528e70450d6
Santilín <software@noviolento.es>
2024-11-24 12:26:43 +0100

    CrudController::returnto: to can be 'returnto'

--- src/CrudController.php
+++ src/CrudController.php
@@ -371,12 +371,15 @@ class CrudController extends \yii\web\Controller
 
 	protected function returnTo(?string $to, string $from, $model, array $redirect_params = []): string|array
 	{
-		$returnTo = $this->request->post('returnTo');
-		if( !$returnTo ) {
-			$returnTo = $this->request->queryParams['returnTo']??null;
-		}
-		if( $returnTo ) {
-			return $returnTo;
+		if ($to == 'returnTo') {
+			$returnTo = $this->request->post('returnTo');
+			if( !$returnTo ) {
+				$returnTo = $this->request->queryParams['returnTo']??null;
+			}
+			if( $returnTo ) {
+				return $returnTo;
+			}
+			$to = null;
 		}
 		if (empty($to)) {
 			if ($this->isJunctionModel && $this->getMasterModel()) {
