e4827ed27067d68e8540cb110ec30f3ce642f979
Santilín <software@noviolento.es>
2024-12-01 21:29:07 +0100

    passed Model common function applyScopes to ModelTrait.php

---
+++ src/models/ModelTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace santilin\churros\models;
+
+use yii\db\ActiveQuery;
+
+trait ModelTrait
+{
+
+	/**
+	 * Applies one or more scopes to an ActiveQuery object, adding a default order if none set.
+	 *
+	 * @param ActiveQuery $q The ActiveQuery object to apply scopes to.
+	 * @param string|array $scopes A single scope (as a string) or an array of scopes to apply.
+	 *                             Each scope can be a string (function name) or an array (function name with arguments).
+	 * @return ActiveQuery The modified ActiveQuery object with applied scopes.
+	 *
+	 */
+	static public function applyScopes(ActiveQuery $q, string|array|null $scopes, bool $set_order_by = true): ActiveQuery
+	{
+		if (!empty($scopes)) {
+			$save_order = $q->orderBy;
+			foreach ( (array)$scopes as $scope) {
+				$scope_args = [];
+				if (is_array($scope)) {
+					$scope_func = trim(reset($scope));
+					$scope_args = $scope;
+				} else {
+					$scope_func = trim($scope);
+				}
+				if( $scope_func ) {
+					call_user_func_array([$q,$scope_func],$scope_args);
+				}
+			}
+			if ($set_order_by && empty($save_order) && $q->orderBy == $save_order) {
+				$q->defaultOrder();
+			}
+		} else if ($set_order_by) {
+			$q->defaultOrder();
+		}
+		return $q;
+	}
+
+
+} // trait
