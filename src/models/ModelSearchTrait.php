6864500ac84c105cf6780b1e19ab7db7a5894695
Santilín <software@noviolento.es>
2025-10-21 07:46:11 +0200

    ModelSearchTrait: orderBy in grid groups fixed

--- src/models/ModelSearchTrait.php
+++ src/models/ModelSearchTrait.php
@@ -157,9 +157,9 @@ trait ModelSearchTrait
 			list($sort_fldname, $table_alias, $model, $relation) = $this->addRelatedFieldToJoin(
 				$col_name, $provider->query);
 			if (isset($_GET['_sort']["-{$col_name}"])) {
-				$provider->query->orderBy([$col_name, SORT_DESC]);
+				$provider->query->orderBy([$col_name => SORT_DESC]);
 			} else { // even if no present
-				$provider->query->orderBy([$col_name, SORT_ASC]);
+				$provider->query->orderBy([$col_name => SORT_ASC]);
 			}
 		}
 		/// @todo move to DataProvider count()?
